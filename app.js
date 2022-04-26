4255cd165b23a7da51138662ce5fb6778906ed7c
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-04-26 22:31:01 +0100

    implement basic adding functionality

---
+++ app.js
@@ -0,0 +1,33 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo)
+
+// Functions
+function addTodo(event) {
+    // prevent form from submitting
+    event.preventDefault()
+    // create todo div
+    const todoDiv = document.createElement('div')
+    todoDiv.classList.add('todo')
+    // create li
+    const newTodo = document.createElement('li')
+    newTodo.innerText = 'hey'
+    newTodo.classList.add('todo-item')
+    todoDiv.appendChild(newTodo)
+    // create checkmark button
+    const completedButton = document.createElement('button')
+    completedButton.innerHTML = '<i class="fa-solid fa-check"></i>'
+    completedButton.classList.add('complete-btn')
+    todoDiv.appendChild(completedButton)
+    // create delete button
+    const trashButton = document.createElement('button')
+    trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>'
+    trashButton.classList.add('complete-btn')
+    todoDiv.appendChild(trashButton)
+    // append to list
+    todoList.appendChild(todoDiv)
+}
