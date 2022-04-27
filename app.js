f936a9608c8ee618fd7be5b36ecfb11643ae0724
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-04-27 19:58:29 +0100

    implementing filter

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
+const filterOption = document.querySelector(".filter-todo")
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo)
 todoList.addEventListener('click', deleteCheck)
+filterOption.addEventListener('click', filterTodo)
 
 // Functions
 function addTodo(event) {
@@ -51,4 +53,33 @@ function deleteCheck(event) {
         const todo = item.parentElement
         todo.classList.toggle("completed")
     }
+}
+
+function filterTodo(event) {
+    const todos = todoList.childNodes;
+    // console.log(todos)
+    todos.forEach(function (todo) {
+        // console.log(todo)
+        if (todo.nodeName !== "#text") {
+            switch (event.target.value) {
+                case "all":
+                    todo.style.display = 'flex'
+                    break;
+                case "complete":
+                    if (todo.classList.contains('completed')) {
+                        todo.style.display = 'flex'
+                    } else {
+                        todo.style.display = 'none'
+                    }
+                    break;
+                case "incomplete":
+                    if (!todo.classList.contains('completed')) {
+                        todo.style.display = 'flex'
+                    } else {
+                        todo.style.display = 'none'
+                    }
+                    break;
+            }
+        }
+    })
 }
